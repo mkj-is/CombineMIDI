694fef845504d4a29d3af948d155acdf989cb3e5
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2022-01-25 10:17:35 +0100

    Improve reading of messages and keeping internal state

--- Sources/CombineMIDI/MIDIStream.swift
+++ Sources/CombineMIDI/MIDIStream.swift
@@ -6,12 +6,20 @@ struct MIDIStream {
     private var port = MIDIPortRef()
     
     init(client: MIDIClient, receive: @escaping (MIDIMessage) -> Void) {
+        // Currently read partial message
+        var currentMessage = MIDIMessage()
+        
         MIDIInputPortCreateWithBlock(client.client, client.generatePortName() as CFString, &port) { pointer, _ in
             pointer
                 .unsafeSequence()
                 .flatMap { $0.sequence() }
-                .chunked(into: 3)
-                .compactMap(MIDIMessage.init)
+                .reduce(into: [MIDIMessage]()) { messages, byte in
+                    currentMessage.bytes.append(byte)
+                    if currentMessage.isComplete {
+                        messages.append(currentMessage)
+                        currentMessage = MIDIMessage()
+                    }
+                }
                 .forEach(receive)
         }
         for i in 0...MIDIGetNumberOfSources() {
